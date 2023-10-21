71b739117d695f3cfab982bc6886983f1d189317
Antony Ishchenko <antony.ishchenko@gmail.com>
2023-10-21 21:57:22 +0300

    completed

--- js/task-2.js
+++ js/task-2.js
@@ -1,14 +1,40 @@
 'use strick';
-//Задача 2. Композиція масивів
+//Задача 2. Розрахунок калорій
 
-function makeArray(firstArray, secondArray, maxLength) {
-  let rezArray = firstArray.concat(secondArray);
-  return rezArray.length <= maxLength ? rezArray : rezArray.slice(0, maxLength);
+function calcAverageCalories(days) {
+  let sum = 0;
+  let count = 0;
+
+  for (let dayRecord of days) {
+    sum += dayRecord.calories;
+    count += 1;
+  }
+
+  return count > 0 ? Math.round(sum / count) : 0;
 }
 
-console.log(makeArray(['Mango', 'Poly'], ['Ajax', 'Chelsea'], 3)); // ["Mango", "Poly", "Ajax"]
-console.log(makeArray(['Mango', 'Poly', 'Houston'], ['Ajax', 'Chelsea'], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
-console.log(makeArray(['Mango'], ['Ajax', 'Chelsea', 'Poly', 'Houston'], 3)); // ["Mango", "Ajax", "Chelsea"]
-console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 2)); // ["Earth", "Jupiter"]
-console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
-console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus', 'Venus'], 0)); // []
+console.log(
+  calcAverageCalories([
+    { day: 'monday', calories: 3010 },
+    { day: 'tuesday', calories: 3200 },
+    { day: 'wednesday', calories: 3120 },
+    { day: 'thursday', calories: 2900 },
+    { day: 'friday', calories: 3450 },
+    { day: 'saturday', calories: 3280 },
+    { day: 'sunday', calories: 3300 },
+  ])
+); // 3180
+
+console.log(
+  calcAverageCalories([
+    { day: 'monday', calories: 2040 },
+    { day: 'tuesday', calories: 2270 },
+    { day: 'wednesday', calories: 2420 },
+    { day: 'thursday', calories: 1900 },
+    { day: 'friday', calories: 2370 },
+    { day: 'saturday', calories: 2280 },
+    { day: 'sunday', calories: 2610 },
+  ])
+); // 2270
+
+console.log(calcAverageCalories([])); // 0
